3331d6d11d4e3d361b63628ea71b663580142b61
hasansami75 <mehedihasan3175@gmail.com>
2021-03-08 15:08:58 +0600

    first commit

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Friend = (props) => {
+    const {name, email, id} = props.friend;
+    const friendStyle = {
+        border: '1px solid purple',
+        margin: '20px',
+        padding: '20px',
+        borderRadius: '20px'
+    }
+    return (
+        <div style={friendStyle}>
+            <h2>Name: {name}</h2>
+            <p>Email: {email}</p>
+            <Link to={`/friend/${id}`}><button>Show detail of {id}</button></Link>
+        </div>
+    );
+};
+
+export default Friend;
